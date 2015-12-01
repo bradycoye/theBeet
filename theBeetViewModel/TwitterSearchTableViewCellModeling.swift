b6fd237956253aa3bd9f8710f983fbf6eacaf570
bradycoye <bradycoye@bradycoye.com>
2015-11-30 22:16:43 -0500

    errors fixed, app crashed on table view

---
+++ theBeetViewModel/TwitterSearchTableViewCellModeling.swift
@@ -0,0 +1,9 @@
+//
+//  TwitterSearchTableViewCellModeling.swift
+//  theBeet
+//
+//  Created by Brady Coye on 11/30/15.
+//  Copyright © 2015 DaemonDevelopment. All rights reserved.
+//
+
+import Foundation
